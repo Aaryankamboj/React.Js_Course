e55e4372cf4c20fd91cd45574e6a730d1f852d1f
Aaryan <a.k.65983.586@gmail.com>
2022-11-27 16:13:56 +0530

    React router dom first lecture understood

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,12 +1,19 @@
 // import logo from './logo.svg';
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 import About from './components/About';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Link
+} from "react-router-dom";
+
 function App() {
-  const [mode, setMode]=useState('dark');
+  const [mode, setMode] = useState('dark');
 
   /*
   const [mode, setMode]=useState({
@@ -24,9 +31,9 @@ function App() {
  }
  */
   const [alert, setalert] = useState(null);
-  const showAlert = (message, type)=>{
+  const showAlert = (message, type) => {
     setalert({
-      msg: message, type: type 
+      msg: message, type: type
     })
     setTimeout(() => {
       setalert(null);
@@ -35,9 +42,9 @@ function App() {
   }
   const [text, setText] = useState("Dark");
   const toggleMode = () => {
-    if(mode==='light'){
+    if (mode === 'light') {
       setMode('dark');
-      document.body.style.backgroundColor='black';
+      document.body.style.backgroundColor = 'black';
       showAlert("Dark Mode has been enabled", "success")
       document.title = "TextUtils - Dark Mode";
       setText('Light');
@@ -48,10 +55,10 @@ function App() {
       setInterval(() => {
         document.title = "TextUtils download karo";
       }, 1500);
-    }      
-    else{
+    }
+    else {
       setMode('light');
-      document.body.style.backgroundColor='white';
+      document.body.style.backgroundColor = 'white';
       showAlert("Light Mode has been enabled", "success");
       document.title = "TextUtils - Light Mode";
 
@@ -62,31 +69,37 @@ function App() {
 
   const [text2, setText2] = useState("light");
   const [noBlue, toBlue] = useState("light");
-  const toggleBlue= ()=>{
-    if(noBlue ==='blue'){
+  const toggleBlue = () => {
+    if (noBlue === 'blue') {
       toBlue('light');
-      document.body.style.backgroundColor='white';
+      document.body.style.backgroundColor = 'white';
       setText2("Blue")
       // showAlert("Blue Mode has been enabled", "success");
     }
-    else{
+    else {
       toBlue('#85aae1');
-      document.body.style.backgroundColor='#85aae1';
+      document.body.style.backgroundColor = '#85aae1';
       setText2("Light");
       // showAlert("Blue Mode has been disabled", "warning");
     }
   }
-    return (
+  return (
     <>
-      <Navbar title="TextUtils" aboutText="AboutTextUtils" mode={mode} noBlue={noBlue}  text={text} text2={text2} toggleMode={toggleMode} toggleBlue={toggleBlue}/>
-      <Alert alert={alert} mode={mode} />
-      {/* <Navbar/> */}
-      <div className="container my-3">
-        {/* <TextForm heading="Enter the text to analyze below" mode={mode}showAlert={showAlert}/> */}
-      <About/>
-      </div>
+      <Router>
+        <Navbar title="TextUtils" aboutText="AboutTextUtils" mode={mode} noBlue={noBlue} text={text} text2={text2} toggleMode={toggleMode} toggleBlue={toggleBlue} />
+        <Alert alert={alert} mode={mode} />
+        {/* <Navbar/> */}
+        <div className="container my-3">
+          <Routes>
+            <Route path="/about" element={<About/>}>
+            </Route>
 
+            <Route path="/" element={<TextForm heading="Enter the text to analyze below" mode={mode} showAlert={showAlert} />}>
+            </Route>
 
+          </Routes>
+        </div>
+      </Router>
     </>
   );
 }
